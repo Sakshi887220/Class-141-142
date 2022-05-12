42ea072080f28440602b2c49c0973e33936f9e24
Sakshi <70653660+Sakshi887220@users.noreply.github.com>
2022-05-12 15:17:58 +0530

    Update content_filtering.py

--- content_filtering.py
+++ content_filtering.py
@@ -3,13 +3,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('final.csv')
-df = df[df['soup'].notna()]
 
-count = CountVectorizer(stop_words='english')
-count_matrix = count.fit_transform(df['soup'])
 
-cosine_sim = cosine_similarity(count_matrix, count_matrix)
 
 df = df.reset_index()
 indices = pd.Series(df.index, index=df['title'])
